e03ccef5aaf5b4d6cc421b7582fa70e68938f47b
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-06-06 04:31:22 +0530

    changes

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ const changeTurn = () => {
 const checkwin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
-        [0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ]
     wins.forEach((e) => {
         if ((boxtext[e[0]].innerHTML === boxtext[e[1]].innerHTML) && (boxtext[e[1]].innerHTML === boxtext[e[2]].innerHTML) && (boxtext[e[0]].innerHTML !== '')) {
@@ -29,8 +29,8 @@ const checkwin = () => {
             IsgameOver = true;
             music.play();
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector('.line').style.width='20vw'
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            // document.querySelector('.line').style.width='20vw'
+            // document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
         }
     })
   
@@ -64,7 +64,7 @@ Reset.addEventListener('click' , ()=>{
     IsgameOver=false;
     document.getElementsByClassName('info')[0].innerHTML = "Turn for " + 'X';
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
-    document.querySelector('.line').style.width='0vw'
+    // document.querySelector('.line').style.width='0vw'
     music.pause();
 })
 
